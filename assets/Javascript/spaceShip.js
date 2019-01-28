facaaf84b91d6d0bcda3d13c46d6842eafb5a21c
ydlx <lichong536@163.com>
2019-01-28 19:04:03 +0800

    OMO弹射器

--- assets/Javascript/spaceShip.js
+++ assets/Javascript/spaceShip.js
@@ -1,12 +1,6 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+// 要干什么
+// 飞碟 omo 线
+// 飞碟移动 屏幕点击 omo移动 飞碟与omo连线 捕获检测
 
 cc.Class({
     extends: cc.Component,
@@ -16,9 +10,16 @@ cc.Class({
     },
 
     // LIFE-CYCLE CALLBACKS:
-
     onLoad() {
-        console.log('spaceShip')
+        console.log('main')
+        var slef = this;
+        var SHIP = slef.SHIP = this.node.getChildByName("ship");
+        var LINE = slef.LINE = this.node.getChildByName("line").getComponent(cc.Graphics);
+        var OMO = slef.OMO = this.node.getChildByName("omo");
+
+        this.FLAG = true;
+
+        this.OMO.runAction(cc.hide());
         // 飞船长大
         var seq = cc.sequence(
             cc.scaleTo(.2, .2),
@@ -27,23 +28,91 @@ cc.Class({
             cc.scaleTo(.8, .8),
             cc.scaleTo(1, 1),
         );
-        
-        this.node.runAction(seq);
 
+        SHIP.runAction(seq);
+
+        // 1.飞碟移动
         // 监听鼠标位置,移动飞船位置
-        this.node.parent.on(cc.Node.EventType.MOUSE_MOVE, function (event) {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
             var location = event.getLocation();
-            var pos = this.node.parent.convertToNodeSpaceAR(location);
-            this.node.setPosition(pos.x, -150);
+            var pos = this.node.convertToNodeSpaceAR(location);
+            this.SHIP.setPosition(pos.x, -150);
+
+            if (this.FLAG) {
+                this.FLAG = false;
+                this.launchOMO(pos);
+            }
+
+        }, this);
+
+        this.node.on(cc.Node.EventType.MOUSE_MOVE, function (event) {
+            var location = event.getLocation();
+            var pos = this.node.convertToNodeSpaceAR(location);
+
+            this.SHIP.setPosition(pos.x, -150);
+
+            if (this.FLAG) {
+                this.OMO.setPosition(pos.x, -150);
+            }
+
+            pos.y = -150;
+            this.moveToPos = pos;
         }, this);
 
     },
 
-
-    getNodePos: function (curNode, targetNode) {
-        var worldPos = curNode.parent.convertToNodeSpaceAR(curNode.position);
-        var pos = targetNode.convertToNodeSpaceAR(worldPos);
-        return pos;
+    // 2.屏幕点击
+    // 
+    launchOMO(worldPos) {
+        var action = cc.sequence(cc.show(), cc.moveTo(2, cc.v2(worldPos.x, worldPos.y)), cc.callFunc(this.retakeOMO, this));
+        this.OMO.runAction(action);
     },
 
+    retakeOMO() {
+        this.isRetake = true;
+        this.OMO.angle = 180;
+    },
+
+    // getNodePos: function (curNode, targetNode) {
+    //     var worldPos = curNode.parent.convertToNodeSpaceAR(curNode.position);
+    //     var pos = targetNode.convertToNodeSpaceAR(worldPos);
+    //     return pos;
+    // },
+    drawLine(shipPos, omoPos) {
+        this.LINE.clear();
+        this.LINE.moveTo(shipPos.x, shipPos.y);
+        this.LINE.lineTo(omoPos.x, omoPos.y);
+        this.LINE.stroke();
+    },
+
+    update(dt) {
+        if (!this.FLAG) {
+            this.drawLine(this.SHIP, this.OMO);
+        }
+
+        if (this.isRetake) {
+            var oldPos = this.OMO.position;
+            var direction = this.moveToPos.sub(oldPos).normalize();
+            var newPos = oldPos.add(direction.mul(100 * dt));
+            this.OMO.setPosition(newPos);
+
+            this.drawLine(this.SHIP, this.OMO);
+            var gap = Math.pow(Math.abs(oldPos.x - newPos.x), 2) + Math.pow(Math.abs(-150 - newPos.y), 2);
+            if (gap < 10) {
+                this.OMO.runAction(cc.hide());
+                this.isRetake = false;
+                this.FLAG = true;
+            }
+        }
+    },
+
+    onEnable: function () {
+        cc.director.getCollisionManager().enabled = true;
+        cc.director.getCollisionManager().enabledDebugDraw = true;
+    },
+
+    onDisable: function () {
+        cc.director.getCollisionManager().enabled = false;
+        cc.director.getCollisionManager().enabledDebugDraw = false;
+    },
 });
